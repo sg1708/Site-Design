06adae149c9d2b6012d5888f087e089a9add79b7
Shevy <shevy.gupta@gmail.com>
2025-06-06 17:53:00 +1000

    modified and added user flow files. Segregated files into more easily manageable pages(like css). Began roughly designing nav bar and components

--- Java/main.js
+++ Java/main.js
@@ -1,3 +1,19 @@
+document.getElementById('searchBtn').addEventListener('click', () => {
+  document.getElementById('searchPopup').classList.toggle('hidden');
+});
+
+document.getElementById('productsBtn').addEventListener('click', () => {
+  document.getElementById('productsPopup').classList.toggle('hidden');
+});
+
+document.getElementById('cartBtn').addEventListener('click', () => {
+  document.getElementById('cartPopup').classList.toggle('hidden');
+});
+
+
+
 function addToCart() {
   alert("Product added to cart!");
 }
+
+
